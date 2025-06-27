ff4af6a211faa950ff026fc9bbdc383b6b0f1a98
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-27 23:33:01 +0600

    hover added

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ return(
     
     <nav className='w-[100%] bg-red-500 p-[20px]'>
        <ul className=' flex justify-center items-center gap-7 text-[30px]'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a className='hover:text-white duration-[.9s]' href="#">Home</a></li>
+          <li><a className='hover:text-white duration-[.9s]' href="#">About</a></li>
+          <li><a className='hover:text-white duration-[.8s]' href="#">Contact</a></li>
        </ul>
     </nav>
     
