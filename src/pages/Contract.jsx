4b8a000139eca8f7985a6eb1d8d4a505ed798949
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-27 23:43:53 +0600

    contact page site ready

--- src/pages/Contract.jsx
+++ src/pages/Contract.jsx
@@ -5,7 +5,7 @@ import react from 'react'
 const Contract =()=>{
 
 return(
-    <div></div>
+       <div className='w-full h-screen bg-green-300 text-[100px] flex justify-center items-center'>Contact With us</div>
 )
 
 }
